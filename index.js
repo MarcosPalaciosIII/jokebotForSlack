0d77e2d4bac107f555e6c23f4e631b0a2e091ee5
Marcos <mp2772@gmail.com>
2019-08-08 23:00:28 -0400

    removed unworking code

--- index.js
+++ index.js
@@ -67,53 +67,53 @@ let runHelp = (sendToChannel) => {
 }
 
 
-let gameOptions = (sendToChannel) => {
-  const params = {
-    icon_emoji: ':thinking_face:'
-  };
-
-  bot.postMessageToChannel({
-    'channel': `${sendToChannel}`,
-    "text": "Would you like to play a game?",
-    "attachments": [
-        {
-            "text": "Choose a game to play",
-            "fallback": "You are unable to choose a game",
-            "callback_id": "wopr_game",
-            "color": "#3AA3E3",
-            "attachment_type": "default",
-            "actions": [
-                {
-                    "name": "game",
-                    "text": "Chess",
-                    "type": "button",
-                    "value": "chess"
-                },
-                {
-                    "name": "game",
-                    "text": "Falken's Maze",
-                    "type": "button",
-                    "value": "maze"
-                },
-                {
-                    "name": "game",
-                    "text": "Thermonuclear War",
-                    "style": "danger",
-                    "type": "button",
-                    "value": "war",
-                    "confirm": {
-                        "title": "Are you sure?",
-                        "text": "Wouldn't you prefer a good game of chess?",
-                        "ok_text": "Yes",
-                        "dismiss_text": "No"
-                    }
-                }
-            ]
-        }
-    ],
-    params
-});
-};
+// let gameOptions = (sendToChannel) => {
+//   const params = {
+//     icon_emoji: ':thinking_face:'
+//   };
+//
+//   bot.postMessageToChannel({
+//     'channel': `${sendToChannel}`,
+//     "text": "Would you like to play a game?",
+//     "attachments": [
+//         {
+//             "text": "Choose a game to play",
+//             "fallback": "You are unable to choose a game",
+//             "callback_id": "wopr_game",
+//             "color": "#3AA3E3",
+//             "attachment_type": "default",
+//             "actions": [
+//                 {
+//                     "name": "game",
+//                     "text": "Chess",
+//                     "type": "button",
+//                     "value": "chess"
+//                 },
+//                 {
+//                     "name": "game",
+//                     "text": "Falken's Maze",
+//                     "type": "button",
+//                     "value": "maze"
+//                 },
+//                 {
+//                     "name": "game",
+//                     "text": "Thermonuclear War",
+//                     "style": "danger",
+//                     "type": "button",
+//                     "value": "war",
+//                     "confirm": {
+//                         "title": "Are you sure?",
+//                         "text": "Wouldn't you prefer a good game of chess?",
+//                         "ok_text": "Yes",
+//                         "dismiss_text": "No"
+//                     }
+//                 }
+//             ]
+//         }
+//     ],
+//     params
+//   });
+// };
 
 
 // Tell a Chuck Norris Joke
